2f3ede1ef1870546356059f733195175846a2fbc
AmaGlug-Glug <78841694+LostNigerianUncle@users.noreply.github.com>
2026-09-10 09:57:52 +1000

    Remove comment from orbitalconstants.py
    Removed unnecessary comment to streamline code.

--- orbitalconstants.py
+++ orbitalconstants.py
@@ -1,5 +1,4 @@
 # Physical constants for the orbital simulator
-# Keeping these in one place means every other file can import what it needs, instead of retyping magic numbers everywhere.
 
 MU_EARTH = 3.986004418e14  # m^3/s^2, standard gravitational parameter for Earth
 
