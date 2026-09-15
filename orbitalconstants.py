2c61da53b13e93605e4d5a1f1cae74a68c5e747a
AmaGlug-Glug <78841694+LostNigerianUncle@users.noreply.github.com>
2026-09-16 09:34:48 +1000

    Add Moon's gravitational parameters and radius

--- orbitalconstants.py
+++ orbitalconstants.py
@@ -3,3 +3,7 @@
 MU_EARTH = 3.986004418e14  # m^3/s^2, standard gravitational parameter for Earth
 
 R_EARTH = 6.371e6  # m, mean radius of Earth
+
+MU_MOON = 4.9048695e12 # m^3/s^2, standard gravitational parameter for Moon
+
+R_MOON = 1.7374e6 # m, mean radius of Moon
